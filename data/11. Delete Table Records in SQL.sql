7bfcce8a158b359c0446b2c677de8383ccb0d1e8
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-02-26 18:56:24 +0700

    Add files via upload

---
+++ data/11. Delete Table Records in SQL.sql
@@ -0,0 +1,10 @@
+# How to Use Delete Statement in SQL
+# Deleting the Table Records
+
+USE FAZA;
+
+
+DELETE FROM EMPLOYEES WHERE ID = 1; # This will deleted data from Employees column where ID = 1
+DELETE FROM EMPLOYEES; # All the records will be deleted
+# DROP is used to delete Tables
+# Delete is used to delete Records from the tables, that's the difference
