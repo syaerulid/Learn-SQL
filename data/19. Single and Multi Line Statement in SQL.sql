be708c2ff06d383021b775cd9bf9fb8f89d7a8ab
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-02-26 22:42:10 +0700

    Add files via upload

---
+++ data/19. Single and Multi Line Statement in SQL.sql
@@ -0,0 +1,10 @@
+# Single Line and Multi Line Statement in SQL
+-- This SQL Statement below is for selecting the database, on which we are going to perform the operations
+USE FAZA;
+
+/* This below two SQL Statements will perform the below operations:
+The first statement is for displaying the tables under 'use statement' before
+The second SQL statement is for retrieving all the records from the employees table
+SHOW TABLES;
+SELECT *
+FROM EMPLOYEES */
