df4ab28cdc4d68cdb358567d02520b826c955532
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-02-28 23:13:11 +0700

    Add files via upload

---
+++ 5.6 Delimiter in SQL.sql
@@ -0,0 +1,13 @@
+# Delimiter in SQL
+
+# Usually we use Delimiter with Semicolon ;
+# But we can use this too other symbols too
+
+USE WORLD;
+Delimiter //
+SELECT *
+FROM CITY//
+
+Delimiter $$
+SELECT *
+FROM CITY$$
